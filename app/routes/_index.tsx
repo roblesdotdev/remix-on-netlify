b31211063f40b61372980caced186ef025c10877
roblesdotdev <httpsnip@gmail.com>
2024-09-01 22:35:00 -0300

    testing envs

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,7 +1,17 @@
+import { useLoaderData } from '@remix-run/react'
+
+export function loader() {
+  return {
+    ENV: process.env,
+  }
+}
+
 export default function Index() {
+  const { ENV } = useLoaderData<typeof loader>()
   return (
     <div className="py-8">
       <h1 className="text-xl">Working...</h1>
+      <pre>{JSON.stringify(ENV, null, 2)}</pre>
     </div>
   )
 }
--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,19 @@
 import { netlifyPlugin } from '@netlify/remix-adapter/plugin'
 import { vitePlugin as remix } from '@remix-run/dev'
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import tsconfigPaths from 'vite-tsconfig-paths'
 
-export default defineConfig({
-  plugins: [remix(), netlifyPlugin(), tsconfigPaths()],
-})
+export default ({ mode }: { mode: string }) => {
+  const env = loadEnv(mode, process.cwd())
+
+  return defineConfig({
+    build: {
+      cssMinify: mode === 'production',
+      sourcemap: true,
+    },
+    plugins: [remix(), netlifyPlugin(), tsconfigPaths()],
+    define: {
+      'process.env': env,
+    },
+  })
+}
